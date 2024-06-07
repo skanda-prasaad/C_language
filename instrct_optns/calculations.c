6416ca0699e563278b8dd7f7cb4cefec100abda4
Skanda Prasad <skandaprasad595@gmail.com>
2024-06-07 11:50:17 +0530

    updated..

---
+++ instrct_optns/calculations.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a = 3.0 / 8;
+    printf("value is : %d", a);
+    return 0;
+}
+
+// Here float * float = float
+// int * flaot = int
+// int * int = int
