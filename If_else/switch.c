361e092da4b164316c375ccde74a25e7f8c923cf
Skanda Prasad <skandaprasad595@gmail.com>
2024-06-08 21:56:13 +0530

    updated..

---
+++ If_else/switch.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int age = 22;
+    switch (age)
+    {
+    case 21:
+        printf("age is 21");
+        break;
+    case 22:
+        printf("age is 22");
+        break;
+    default:
+        break;
+    }
+}
