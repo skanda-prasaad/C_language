91bbd74a06182214531d4dadc408864b059aaa6a
Skanda Prasad <skandaprasad595@gmail.com>
2024-06-09 21:57:24 +0530

    updared.c

---
+++ loops/increment.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int i = 5;
+    printf("%d\n", i++);
+    printf("%d", i);
+}
+
+#include <stdio.h>
+int main()
+{
+    int i = 5;
+    printf("%d\n", ++i);
+    printf("%d", i);
+}
---
+++ loops/problems/initial_0.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int i = 0;
+    while (i < 21)
+    {
+        if (i >= 10)
+        {
+            printf("The value is %d\n", i);
+        }
+        i++;
+    }
+}
